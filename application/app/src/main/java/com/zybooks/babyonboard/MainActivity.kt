2340cce420639789ddf024d8e64964abb4eba180
sddao17 <54093435+sddao17@users.noreply.github.com>
2022-07-29 22:25:05 -0700

    Updated Disclaimer fragment

--- application/app/src/main/java/com/zybooks/babyonboard/MainActivity.kt
+++ application/app/src/main/java/com/zybooks/babyonboard/MainActivity.kt
@@ -8,22 +8,20 @@ import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
 
-        /*
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
         val appBarConfig = AppBarConfiguration.Builder(
-            R.id.navigation_game, R.id.navigation_color, R.id.navigation_help).build()
+            R.id.navigation_help, R.id.navigation_disclaimer, R.id.navigation_help).build()
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
         NavigationUI.setupWithNavController(navView, navController)
-         */
     }
 }
